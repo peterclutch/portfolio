adcda0502b9c08b6d29668f8b85f37df81c58ae3
PeterClutch <peterhallum@hotmail.com>
2021-03-03 13:29:58 +0800

    hopefully clip path works on mobile now

--- src/app/modules/bio/bio.component.ts
+++ src/app/modules/bio/bio.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ph-bio',
   templateUrl: './bio.component.html',
   styleUrls: ['./bio.component.scss', './bio-intro.component.scss']
 })
-export class BioComponent implements OnInit {
+export class BioComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
